a5a43fa3ea940623a3c9702a11a2dfcb6b1d485c
porkmanic <nbliuhaobo@gmail.com>
2020-03-13 20:41:29 +0800

    Update script.js
    fs

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 const second = 1000,
-      minute = second * 60,
-      hour = minute * 60,
-      day = hour * 24;
+  minute = second * 60,
+  hour = minute * 60,
+  day = hour * 24;
 
-let countDown = new Date('Mar 27, 2020 00:00:00').getTime(),
-    x = setInterval(function() {    
 
-      let now = new Date().getTime(),
-          distance = countDown - now;
+let countDown = new Date('Mar 14, 2020 00:00:00').getTime();
+let now = new Date().getTime(),
+  distance = countDown - now;
+  if (Math.floor(distance / (day)) <= 0
+  && Math.floor((distance % (day)) / (hour)) <=0
+  && Math.floor((distance % (hour)) / (minute)) <= 0
+  && Math.floor((distance % (minute)) / second) <= 0) {
+    window.location.href="https://porkmanic.github.io/birthd/2/";
+  }
+function x () {
 
-      document.getElementById('days').innerText = Math.floor(distance / (day)),
-        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
-        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
-        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+  let now = new Date().getTime(),
+    distance = countDown - now;
 
-      //do something later when date is reached
-      //if (distance < 0) {
-      //  clearInterval(x);
-      //  'IT'S MY BIRTHDAY!;
-      //}
+  document.getElementById('days').innerText = Math.floor(distance / (day)),
+    document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+    document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+    document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
 
-    }, second)
+  //do something later when date is reached
+  //if (distance < 0) {
+  //  clearInterval(x);
+  //  'IT'S MY BIRTHDAY!;
+  //}
+
+}
+x()
+setInterval(x,second)
