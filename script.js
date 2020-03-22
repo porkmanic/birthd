1e398058411be0edcf7fedfe8486d7cc191791cc
porkmanic <nbliuhaobo@gmail.com>
2020-03-22 17:31:42 +0800

    Add files via upload
    啊啊

--- script.js
+++ script.js
@@ -3,16 +3,22 @@ const second = 1000,
   hour = minute * 60,
   day = hour * 24;
 
-
-let countDown = new Date('Mar 27, 2020 00:00:00').getTime();
-let now = new Date().getTime(),
-  distance = countDown - now;
-  if (Math.floor(distance / (day)) <= 0
-  && Math.floor((distance % (day)) / (hour)) <=0
-  && Math.floor((distance % (hour)) / (minute)) <= 0
-  && Math.floor((distance % (minute)) / second) <= 0) {
-    window.location.href="https://porkmanic.github.io/birthd/2/";
-  }
+let lmonth = 3
+let lday = 4
+let now1 = new Date()
+// 当前时间的农历
+let lnow = calendar.solar2lunar(now1.getFullYear(),now1.getMonth(),now1.getDate());
+// 生日公历
+let lbj = calendar.lunar2solar(lnow.lYear,lmonth,lday)
+let gongBirth = new Date(lbj.date)
+// 公历生日加一天 超过了 下一年为基准
+if (gongBirth.getTime() + day <= now1.getTime()) {
+  lbj = calendar.lunar2solar(lnow.lYear + 1,lmonth,lday)
+  gongBirth = new Date(lbj.date)
+} else if (now1.getTime() - gongBirth.getTime() > 0 && now1.getTime() - gongBirth.getTime() < day) {
+  window.location.href="https://porkmanic.github.io/birthd/2/";
+}
+let countDown = gongBirth.getTime();
 function x () {
 
   let now = new Date().getTime(),
@@ -22,12 +28,6 @@ function x () {
     document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
     document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
     document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
-
-  //do something later when date is reached
-  //if (distance < 0) {
-  //  clearInterval(x);
-  //  'IT'S MY BIRTHDAY!;
-  //}
 
 }
 window.onload = x
